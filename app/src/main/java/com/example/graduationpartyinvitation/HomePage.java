e7e7b5a0c7028006266f840ce5d38be86512ad08
lydieanna <sh.alqudree@gmail.com>
2023-07-01 23:37:24 +0800

    integration

--- app/src/main/java/com/example/graduationpartyinvitation/HomePage.java
+++ app/src/main/java/com/example/graduationpartyinvitation/HomePage.java
@@ -1,15 +1,34 @@
 package com.example.graduationpartyinvitation;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class HomePage extends AppCompatActivity {
 
     ImageView mReserve, mQR, mLocation;
+    TextView calendarTextView,coordinatesTextView;
+
+    private static final int PERMISSION_REQUEST_CODE = 123;
+    private static final int REQUEST_CALENDAR_PERMISSION = 1;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +37,8 @@ public class HomePage extends AppCompatActivity {
         mReserve = findViewById(R.id.reserveForm);
         mQR = findViewById(R.id.qr);
         mLocation = findViewById(R.id.location);
+        coordinatesTextView = findViewById(R.id.coordinatesTextView);
+        calendarTextView = findViewById(R.id.calendarTextView);
 
         //reservation
         mReserve.setOnClickListener(v -> {
@@ -33,8 +54,40 @@ public class HomePage extends AppCompatActivity {
 
         //map
         mLocation.setOnClickListener(v -> {
-            Intent intent = new Intent(HomePage.this, ReservationForm.class);
+            Intent intent = new Intent(HomePage.this, Map.class);
             startActivity(intent);
         });
+
+        calendarTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCalendar();
+            }
+        });
+
+
+    }
+
+    // Handle the permission request response
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CALENDAR_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                openCalendar();
+            } else {
+                Toast.makeText(this, "Calendar permission denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void openCalendar() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CALENDAR) == PackageManager.PERMISSION_GRANTED) {
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("content://com.android.calendar/time"));
+            startActivity(intent);
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CALENDAR}, REQUEST_CALENDAR_PERMISSION);
+        }
     }
 }
---
+++ app/src/main/java/com/example/graduationpartyinvitation/Map.java
@@ -0,0 +1,50 @@
+package com.example.graduationpartyinvitation;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import android.os.Bundle;
+
+public class Map extends AppCompatActivity implements OnMapReadyCallback {
+    private GoogleMap mMap;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.maps);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        // Assign the provided GoogleMap instance to the member variable mMap
+        mMap = googleMap;
+
+        // Create a LatLng object with the desired latitude and longitude values
+        LatLng latLng = new LatLng(3.5074244876264133, 103.39464588895557); // coordinates
+
+        // Create a MarkerOptions object and set its position and title
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(latLng);
+        markerOptions.title(latLng.latitude + "  " + latLng.longitude);
+
+        // Clear any existing markers on the map
+        mMap.clear();
+
+        // Animate the camera to the specified LatLng position and set the desired zoom level
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 20));
+
+        // Add the marker to the map
+        mMap.addMarker(markerOptions);
+    }
+}
